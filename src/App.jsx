ce1869d8a15c3d6d41a37cc9b06ef8f5ea123317
alimran74 <alimranimran748596@gmail.com>
2025-04-13 17:46:19 +0600

    added daisyui nav

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
 
+import { Suspense } from 'react'
 import './App.css'
 import DaisyNav from './components/DaisyNav/DaisyNav'
 import NavBar from './components/NAvBar/NavBar'
+import PricingOptions from './components/PricingOptions/PricingOptions'
+
+
+const pricingPromise = fetch('pricingData.json').then(res => res.json());
 
 function App() {
   
@@ -12,6 +17,11 @@ function App() {
         <NavBar></NavBar>
         {/* <DaisyNav></DaisyNav> */}
       </header>
+      <main>
+        <Suspense fallback={<span className="loading loading-infinity loading-lg"></span>}>
+          <PricingOptions pricingPromise={pricingPromise}></PricingOptions>
+        </Suspense>
+      </main>
 
 
     
